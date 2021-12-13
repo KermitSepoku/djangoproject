b8d121dee433b98eafb29bf37a286fc93499d2de
KermitSepoku <sanya.chebotarev.01@mail.ru>
2021-12-13 22:54:59 +0600

    Modified templates to display posts from database.

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('search/', views.Search.as_view(), name = 'search'),
+    path('register/', views.register, name = 'register'),
+    path('login/', views.user_login, name = 'login'),
+    path('logout/', views.user_logout, name = 'logout'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,45 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from django.contrib import messages
 from django.views.generic import ListView
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login , logout
+from django.contrib.auth.models import User
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter()
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def register(request):
+	if request.method =='POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			return redirect('post_list')
+	else:
+		form = UserCreationForm() 
+	return render(request, 'blog/login.html',{'form':form})
+
+def user_login(request):
+	if request.method =='POST':
+		form = AuthenticationForm(data = request.POST)
+		if form.is_valid():
+			user = form.get_user()
+			login(request, user)
+			return redirect('post_list')
+	else:
+		form = AuthenticationForm() 
+	return render(request, 'blog/login.html',{'form':form})
+
+def user_logout(request):
+	logout(request)
+	return redirect('login')
 
 class Search(ListView):
 	template_name = 'blog/search.html'
